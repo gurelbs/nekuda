337e4e85a592bc6d732bf59dc4d112b9c3288b48
Gurel <guri240@gmail.com>
2023-07-04 04:25:27 +0300

    add empty page

---
+++ src/routes/search/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { page } from '$app/stores';
+
+    const q = $page.url.searchParams.get('q') ?? '';
+</script>
+
+<h1>
+    hello to search page.
+</h1>
+
+{#if q}
+<p>your query is {q}</p>
+{/if}
